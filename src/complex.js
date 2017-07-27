34dc9f1a714c7e0da7f7cd975de53cf05b206453
Muneeb <arshadm@Muneebs-MBP.home>
2017-07-26 23:48:26 -0400

    initial drop - complex first pass, a-frame

---
+++ src/complex.js
@@ -0,0 +1,41 @@
+const complex = {
+
+    "cj": [
+        { "CJ1":[ "Clean", "FSQ 3", "Jerk" ] },
+        { "CJ1":[ "Hang Clean & Jerk", "Clean & Jerk", "Hang Clean" ] },
+        { "CJ1":[ "High Pull", "Hang Clean", "Jerk 2" ] },
+        { "CJ1":[ "High Pull", "Clean & Jerk" ] },
+        { "CJ1":[ "Power Clean", "Pause FSQ 3 (:03)", "Jerk" ] },
+        { "CJ1":[ "3 Position Pause Clean (:02)", "Clean & Jerk" ] },
+        { "CJ1":[ "Clean & Jerk", "Neck Jerk", "Jerk" ] },
+        { "CJ1":[ "Slow Deadlift", "Clean & Jerk" ] },
+        { "CJ1":[ "Power Clean", "Power Jerk 2" ] },
+        { "CJ1":[ "Clean", "Jerk 3" ] },
+        { "CJ1":[ "Power Clean", "FSQ", "Hang Clean & Jerk" ] },
+        { "CJ2":[ "Hang Clean 3", "Jerk" ] },
+        { "CJ2":[ "Power Clean", "FSQ 5", "Jerk" ] },
+        { "CJ2":[ "Power Clean", "PFSQ 5 (:03)", "Jerk" ] },
+        { "CJ2":[ "Power Clean", "Clean", "Power Jerk", "Jerk" ] },
+        { "CJL":[ "Clean", "FSQ 9", "Jerk" ] }
+
+    ],
+    "sn": [
+        { "SN1":[ "High Pull", "Hang Snatch" ] },
+        { "SN1":[ "High Pull", "3 Position Snatch" ] },
+        { "SN1":[ "Hang Snatch", "Snatch", "Hang Snatch" ] },
+        { "SN1":[ "Snatch", "POHS 3 (:02)" ] },
+        { "SN1":[ "Snatch", "OHS 3", "Snatch" ] },
+        { "SN1":[ "Slow Deadlift", "Snatch" ] },
+        { "SN1":[ "Slow Deadlift", "High Pull", "Snatch" ] },
+        { "SN1":[ "Deadlift to Knees and Hold (:03)", "Snatch" ] },
+        { "SN1":[ "3 Position Snatch 2"] },
+        { "SN1":[ "Power Snatch", "Drop Snatch" ] },
+        { "SN1":[ "Snatch", "Drop Snatch", "OHS 2" ] },
+        { "SN2":[ "Snatch", "OHS", "Drop Snatch" ] },
+        { "SN2":[ "Snatch", "Pause OHS 5 (:03)" ] },
+        { "SN2":[ "Snatch", "Drop Snatch", "Pause OHS 2 (:05)" ] },
+        { "SN2":[ "Power Snatch", "Pause OHS 4 (:05)" ] },
+        { "SNL":[ "Snatch", "OHS 10" ] }      
+    ]
+
+ }
